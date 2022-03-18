162907f2f471bbfbbc714c3fce7f6b876d682307
SenDev001 <golden.peach.ts@gmail.com>
2022-03-19 00:56:40 +0700

    update

--- index.js
+++ index.js
@@ -30,28 +30,50 @@ app.use(cors(corsOptions));
 
 
 app.get("/getAssets/:collection_slug/:offset", async (req, res) => {
-    let offset = req.params.offset , collection_slug = req.params.collection_slug , err_msg = "" ;
+  let offset = req.params.offset , collection_slug = req.params.collection_slug , err_msg = "" ;
 
-    const options = {
-        method: 'GET',
-        url: `https://api.opensea.io/api/v1/assets?collection_slug=${collection_slug}&limit=50&offset=${offset}`,
-        headers: {
-            Accept: 'application/json',
-            "x-api-key": x_api_key
-        }
-    };
-    const result = await axios.request(options)
-    .then( (response) => {
-        return response.data 
-    })
-    .catch((error) => {
-        console.log(error);
-        err_msg = error;
-    });
+  const options = {
+      method: 'GET',
+      url: `https://api.opensea.io/api/v1/assets?collection_slug=${collection_slug}&limit=50&offset=${offset}`,
+      headers: {
+          Accept: 'application/json',
+          "x-api-key": x_api_key
+      }
+  };
+  const result = await axios.request(options)
+  .then( (response) => {
+      return response.data 
+  })
+  .catch((error) => {
+      console.log(error);
+      err_msg = error;
+  });
 
-    if( !err_msg ) res.send(result.assets);
+  if( !err_msg ) res.send(result.assets);
 
 });
+
+app.get("/getListings/:asset_contract_address/:token_id", async (req, res) => {
+  let asset_contract_address = req.params.asset_contract_address , token_id = req.params.token_id ;
+  console.log("###", asset_contract_address , token_id);
+  const options = {
+    method: 'GET',
+    url: `https://api.opensea.io/api/v1/asset/${asset_contract_address}/${token_id}/listings`,
+    headers: {
+      Accept: 'application/json',
+      "x-api-key": x_api_key
+    }
+  };
+  const result = await axios.request(options)
+  .then( (response) => {
+    return response.data 
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  res.send(result);
+})
 
 app.use(express.static('./build'));
 
